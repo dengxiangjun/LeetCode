047f6aabe00eb391c855c003b06829a00a14f2db
dxj <437833417@qq.com>
2017-11-09 21:09:34 +0800

    测0"

---
+++ src/Implement_strStr.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/implement-strstr/description/
+ * Implement strStr().
+
+ Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ Example 1:
+
+ Input: haystack = "hello", needle = "ll"
+ Output: 2
+ Example 2:
+
+ Input: haystack = "aaaaa", needle = "bba"
+ Output: -1
+ */
+public class Implement_strStr {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
