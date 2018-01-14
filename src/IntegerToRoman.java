b5387a383c09556c32a389809acc37f5e05d9891
{dengxiangjun} <{43833417@qq.com}>
2018-01-14 11:18:34 +0800

    int to roman

--- src/IntegerToRoman.java
+++ src/IntegerToRoman.java
@@ -4,37 +4,49 @@ import java.util.Map;
 /**
  * https://leetcode.com/problems/roman-to-integer/description/
  * Given a roman numeral, convert it to an integer.
- * <p/>
+ * <p>
  * Input is guaranteed to be within the range from 1 to 3999.
- * <p/>
- * <p/>
+ * <p>
+ * <p>
  * Created by deng on 2018/1/11.
  */
 public class IntegerToRoman {
     public static void main(String[] args) {
-        int a = 24;
+        int a = 99;
         String result = intToRoman(a);
         System.out.println(result);
     }
 
+//    /**
+//     * 贪心算法
+//     * @param num
+//     * @return
+//     */
+//    public static String intToRoman(int num) {
+//        Map<Integer, String> map = new HashMap<Integer, String>();
+//        int[] a = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+//        String[] s = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+//        StringBuffer sb = new StringBuffer();
+//        for (int i = 0; i< a.length;i++){
+//            while (num > a[i]){
+//                num -= a[i];
+//                sb.append(s[i]);
+//            }
+//        }
+//        return sb.toString();
+//    }
+
+    /**
+     * @param num
+     * @return
+     */
     public static String intToRoman(int num) {
-        Map<Integer, Character> map = new HashMap<Integer, Character>();
-        int[] arr = {1000,500,100,50,10,5,1};
-        map.put(1, 'I');
-        map.put(5, 'V');
-        map.put(10, 'X');
-        map.put(50, 'L');
-        map.put(100, 'C');
-        map.put(500, 'D');
-        map.put(1000, 'M');
-        int tmp = num;
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0;i<arr.length;i++){
-            while (tmp - arr[i] >= 0) {
-                sb.append(map.get(arr[i]));
-                tmp -= arr[i];
-            }
-        }
+        String[] M = {"", "M", "MM", "MMM"};
+        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        StringBuilder sb = new StringBuilder();
+        sb.append(M[num / 1000]).append(C[num % 1000 / 100]).append(X[num % 100 / 10]).append(I[num % 10]);
         return sb.toString();
     }
 }
