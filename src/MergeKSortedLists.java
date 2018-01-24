8fc7ff3ce290f2c2082bd2d701b8659f2c18242f
dxj <437833417@qq.com>
2018-01-24 22:59:38 +0800

    归并多个有序链表,使用堆

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -1,3 +1,6 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/merge-k-sorted-lists/description/
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
@@ -19,64 +22,68 @@ public class MergeKSortedLists {
         }
     }
 
-    public static ListNode mergeKLists(ListNode[] lists) {
-        return mergeKLists(lists, 0, lists.length - 1);
-    }
-
-    public static ListNode mergeKLists(ListNode[] lists, int left, int right) {
-        if (left == right) return lists[left];
-        if (left < right) {
-            int mid = (left + right) / 2;
-            ListNode l1 = mergeKLists(lists, left, mid);
-            ListNode l2 = mergeKLists(lists, mid + 1, right);
-            return merge(l1, l2);
-        }
-        return null;
-    }
-
-    public static ListNode merge(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
-        }
-        prev.next = l1 == null ? l2 : l1;
-        return prehead.next;
-    }
-//    /**
-//     * 超时
-//     * @param lists
-//     * @return
-//     */
 //    public static ListNode mergeKLists(ListNode[] lists) {
-//        if (lists.length == 0) return null;
-//        ListNode preHead = new ListNode(-1), pre = preHead;
-//        boolean flag = true;
-//        while (flag) {
-//            int min = Integer.MAX_VALUE,minIndex = 0;
-//            for (int i = 0; i < lists.length; i++) {
-//                ListNode node = lists[i];
-//                if (node != null && min > node.val){
-//                    min = node.val;
-//                    minIndex = i;
-//                }
-//            }
-//            if (min == Integer.MAX_VALUE) flag = false;
-//            else {
-//                pre.next = new ListNode(min);
-//                pre = pre.next;
-//                lists[minIndex] = lists[minIndex].next;
-//            }
-//        }
-//        return preHead.next;
+//        return mergeKLists(lists, 0, lists.length - 1);
 //    }
+//
+//    public static ListNode mergeKLists(ListNode[] lists, int left, int right) {
+//        if (left == right) return lists[left];
+//        if (left < right) {
+//            int mid = (left + right) / 2;
+//            ListNode l1 = mergeKLists(lists, left, mid);
+//            ListNode l2 = mergeKLists(lists, mid + 1, right);
+//            return merge(l1, l2);
+//        }
+//        return null;
+//    }
+//
+//    public static ListNode merge(ListNode l1, ListNode l2) {
+//        ListNode prehead = new ListNode(-1);
+//        ListNode prev = prehead;
+//        while (l1 != null && l2 != null) {
+//            if (l1.val < l2.val) {
+//                prev.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                prev.next = l2;
+//                l2 = l2.next;
+//            }
+//            prev = prev.next;
+//        }
+//        prev.next = l1 == null ? l2 : l1;
+//        return prehead.next;
+//    }
+
+    /**
+     * 超时
+     *
+     * @param lists
+     * @return
+     */
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        ListNode preHead = new ListNode(-1), pre = preHead;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode l1, ListNode l2) {
+                if (l1.val < l2.val) return -1;
+                else if (l1.val == l2.val) return 0;
+                else return 1;
+            }
+        });
+        for (int i = 0; i < lists.length; i++) {
+            ListNode node = lists[i];
+            if (node != null) queue.add(node);
+        }
+
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            pre.next = node;
+            pre = pre.next;
+            if (node.next != null) queue.add(node.next);
+        }
+        return preHead.next;
+    }
 
     public static class ListNode {
         int val;
