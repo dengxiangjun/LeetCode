576815d0d8382c4da1b8da904a4ebe27393af154
dxj <437833417@qq.com>
2018-01-25 11:22:44 +0800

    交换节点对

---
+++ src/ReverseNodesInKGroup.java
@@ -0,0 +1,54 @@
+/**
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ * <p/>
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ * <p/>
+ * You may not alter the values in the nodes, only nodes itself may be changed.
+ * <p/>
+ * Only constant memory is allowed.
+ * <p/>
+ * For example,
+ * Given this linked list: 1->2->3->4->5
+ * <p/>
+ * For k = 2, you should return: 2->1->4->3->5
+ * <p/>
+ * For k = 3, you should return: 3->2->1->4->5
+ * <p/>
+ * <p/>
+ * Created by deng on 2018/1/25.
+ */
+public class ReverseNodesInKGroup {
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1), n2 = new ListNode(2), n3 = new ListNode(3), n4 = new ListNode(4), n5 = new ListNode(5);
+        n1.next = n2;
+//        n2.next = n3;
+//        n3.next = n4;
+//        n4.next = n5;
+        ListNode result = reverseKGroup(n1, 2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+}
---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,54 @@
+/**
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p/>
+ * For example,
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * <p/>
+ * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ * Created by deng on 2018/1/25.
+ */
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1), n2 = new ListNode(2), n3 = new ListNode(3), n4 = new ListNode(4), n5 = new ListNode(5);
+        n1.next = n2;
+//        n2.next = n3;
+//        n3.next = n4;
+//        n4.next = n5;
+        ListNode result = swapPairs(n1);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode preHead = new ListNode(-1), pre = preHead, cur = head;
+        preHead.next = head;
+        while (cur !=null && cur.next != null) {
+            pre.next = cur.next;
+            cur.next = cur.next.next;
+            pre.next.next = cur;
+            pre = cur;
+            cur = cur.next;
+        }
+        return preHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+}
