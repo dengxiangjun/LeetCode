8e92dd767f02f1a356df2a8b738b957e90f3ce8c
{dengxiangjun} <{43833417@qq.com}>
2018-03-04 16:13:56 +0800

    ValidNumber

--- src/ValidNumber.java
+++ src/ValidNumber.java
@@ -17,16 +17,29 @@
  */
 public class ValidNumber {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String s = "0.1";
         boolean res = isNumber(s);
     }
 
     public static boolean isNumber(String s) {
-        int i = 0;
-        while (i<s.length()){
-            //if (s.charAt(i)>='a')
+        s = s.trim();
+        boolean numberSeen = false, pointSeen = false, eSeen = false, numberAfterE = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                numberSeen = true;
+                numberAfterE = true;
+            } else if (s.charAt(i) == '.') {
+                if (eSeen || pointSeen) return false;
+                pointSeen = true;
+            } else if (s.charAt(i) == 'e') {
+                if (eSeen || !numberSeen) return false;
+                eSeen = true;
+                numberAfterE = false;
+            } else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e') return false;
+            } else return false;
         }
-        return false;
+        return numberSeen && numberAfterE;
     }
 }
