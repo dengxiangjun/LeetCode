bfcbc610aae4765bdbddbd0ca7012a4c9381f559
lucanwudi <1554111572@qq.com>
2018-03-11 17:06:18 +0800

    MinimumWindowSubstring

--- src/MinimumWindowSubstring.java
+++ src/MinimumWindowSubstring.java
@@ -16,11 +16,47 @@
 public class MinimumWindowSubstring {
 
     public static void main(String[] args) {
-        String res = minWindow("ADOBECODEBANC","ABC");
+        String res = minWindow("ABBCD", "ABC");
         System.out.println(res);
     }
 
     public static String minWindow(String s, String t) {
-        return "";
+        String res = "";
+        char[] tTable = new char[256];
+        char[] sTable = new char[256];
+        for (int i = 0; i < t.length(); i++) tTable[t.charAt(i)]++;
+        int left = findNextCharInT(0, s, tTable), right = left, matchCnt = 0;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            if (sTable[ch] < tTable[ch]) {//
+                matchCnt++;
+            }
+            sTable[ch]++;
+            while (left < s.length() && matchCnt == t.length()) {//left与right之间找到一个匹配的串，需要移动左指针
+                if (res.isEmpty() || res.length() > right - left + 1) res = s.substring(left, right + 1);//更新res
+                char leftChar = s.charAt(left);
+                if (sTable[leftChar] == tTable[leftChar]) matchCnt--;
+                sTable[leftChar]--;
+                left = findNextCharInT(left + 1, s, tTable);
+            }
+            right = findNextCharInT(right + 1, s, tTable);
+        }
+        return res;
+    }
+
+    /**
+     * 查找下一个S中包含T中字符的位置
+     * @param from
+     * @param s
+     * @param tTable
+     * @return
+     */
+    public static int findNextCharInT(int from, String s, char[] tTable) {
+        int i = from;
+        while (i < s.length()) {
+            if (tTable[s.charAt(i)] > 0) return i;
+            i++;
+        }
+        return i;
     }
 }
