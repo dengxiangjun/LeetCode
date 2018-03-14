614fc73d22a9a678ef93e1949d2c76909cbe8b9c
dxj <437833417@qq.com>
2018-03-15 00:00:04 +0800

    MaximalRectangle

--- src/MaximalRectangle.java
+++ src/MaximalRectangle.java
@@ -14,12 +14,24 @@
 public class MaximalRectangle {
 
     public static void main(String[] args) {
-        int[] a = {2, 1,2};
-        //int res = maximalRectangle(a);
-        //System.out.println(res);
+        char[][] a = {{1, 0, 1, 0, 0}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}};
+        int res = maximalRectangle(a);
+        System.out.println(res);
     }
 
-    public static int maximalRectangle(char[][] matrix) {
+    public static int maximalRectangle(char[][] a) {
+        if (a == null || a.length == 0) return 0;
+        int[][] dp = new int[a.length][a[0].length];
+        dp[0][0] = a[0][0] == 0 ? 0 : 1;
+        for (int i = 1; i < a[0].length; i++) dp[0][i] = a[0][i - 1] == 0 ? dp[0][i - 1] : (dp[0][i - 1] + a[0][i - 1]);
+        for (int i = 1; i < a.length; i++)  dp[i][0] = a[i - 1][0] == 0 ? dp[i - 1][0] : (dp[i - 1][0] + a[i - 1][0]);
+        for (int i = 1; i < a.length; i++)
+            for (int j = 1; j < a[0].length; j++) {
+                if (a[i][j] == 0) dp[i][j] = 0;
+                else {
+                    if (a[i - 1][j] == 0) dp[i][j] = 0;
+                }
+            }
         return 0;
     }
 }
