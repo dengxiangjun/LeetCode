7f5d6169f35ea4cbc988084e273b5ea865b91d7e
lucanwudi <1554111572@qq.com>
2018-03-28 22:41:02 +0800

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -22,8 +22,8 @@ public class Triangle {
 
     public static void main(String[] args) {
         List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-        List<Integer> list0 = new ArrayList<Integer>(),list1 = new ArrayList<Integer>(),
-                list2 = new ArrayList<Integer>(),list3 = new ArrayList<Integer>();
+        List<Integer> list0 = new ArrayList<Integer>(), list1 = new ArrayList<Integer>(),
+                list2 = new ArrayList<Integer>(), list3 = new ArrayList<Integer>();
         list0.add(2);
         list1.add(3);
         list1.add(4);
@@ -68,14 +68,44 @@ public class Triangle {
 //        dfs(triangle,row + 1,col + 1,sum,min);
 //    }
 
+//    /**
+//     * dp O(n*n) extra space
+//     * 自顶向下
+//     * @param triangle
+//     * @return
+//     */
+//    public static int minimumTotal(List<List<Integer>> triangle) {
+//        if (triangle == null || triangle.size() == 0) return 0;
+//        int[][] dp = new int[triangle.size()][triangle.size()];
+//        dp[0][0] = triangle.get(0).get(0);
+//        if (triangle.size() == 1) return dp[0][0];
+//        int min = Integer.MAX_VALUE;
+//        for (int i = 1; i < triangle.size(); i++) {
+//            for (int j = 0; j < triangle.get(i).size(); j++) {
+//                if (j == 0) dp[i][j] = dp[i - 1][j] + triangle.get(i).get(j);
+//                else if (j == triangle.get(i).size() - 1) dp[i][j] = dp[i - 1][j - 1] + triangle.get(i).get(j);
+//                else dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+//                if (i == triangle.size() - 1) min = Math.min(min, dp[i][j]);
+//            }
+//        }
+//        return min;
+//    }
+
     /**
-     * dp
+     * dp O(n) extra space
+     * 自底向上
      * @param triangle
      * @return
      */
     public static int minimumTotal(List<List<Integer>> triangle) {
-        int[] dp = new int[triangle.size() + 1];
-        dp[0] = 0;
-        return 0;
+        if (triangle == null || triangle.size() == 0) return 0;
+        int[]dp = new int[triangle.size() + 1];
+        dp[0] = triangle.get(0).get(0);
+        for (int i = triangle.size() - 1; i >=0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+               dp[j] = Math.min(dp[j],dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
     }
 }
