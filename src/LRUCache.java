a2fcc034f95a7f902da0ebc7bbd35401db171aac
dengxiangjun <437833417@qq.com>
2018-05-12 23:35:26 +0800

    LRUCache设计

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/lru-cache/description/
  * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
@@ -26,15 +29,99 @@
  */
 public class LRUCache {
 
-    public LRUCache(int capacity) {
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+//        cache.put(2, 1);
+//        cache.put(2, 2);
+//        System.out.println(cache.get(2));
+//        cache.put(1, 1);
+//        cache.put(4, 1);
+//        System.out.println(cache.get(2));
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));
+        cache.put(3, 3);
+        System.out.println(cache.get(2));
+        cache.put(4, 4);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+    }
 
+    private Map<Integer, Node> map;
+    private int capacity;
+    private Node head, tail;
+
+    private class Node {
+        public int key;
+        public int val;
+        public Node pre;
+        public Node next;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        this.map = new HashMap<Integer, Node>();
+        this.capacity = capacity;
+        head = null;
+        tail = null;
     }
 
     public int get(int key) {
-
+        Node node = map.get(key);
+        if (node == null) return -1;
+        else {
+            if (node != tail) {
+                if (node != head) {
+                    node.pre.next = node.next;
+                    node.next.pre = node.pre;
+                } else {
+                    head = head.next;
+                }
+                tail.next = node;
+                node.pre = tail;
+                tail = node;
+                node.next = null;
+            }
+        }
+        return node.val;
     }
 
     public void put(int key, int value) {
-
+        Node node = map.get(key);
+        if (node != null) {//已经存在相同的key
+            node.val = value;
+            if (node != tail) {//不是尾结点，需要把这个节点放到链表的尾部
+                if (node != head) {//不是头结点，需要连接该节点的前驱节点和后继结点
+                    node.pre.next = node.next;
+                    node.next.pre = node.pre;
+                } else head = head.next;//是头结点，则头结点指针后移
+                tail.next = node;//链接到节点尾部
+                node.pre = tail;
+                tail = node;
+                node.next = null;
+            }
+        } else {//不存在相同的key，插入到链表尾部
+            node = new Node(key, value);
+            map.put(key, node);//存入hashmap
+            if (capacity == 0) {//如果缓存已经满了
+                map.remove(head.key);//淘汰掉头结点
+                head = head.next;//头指针后移
+                capacity++;//容量置1，表示还可以存入一个元素
+            }
+            if (head == null && tail == null)//链表当前无数据
+                head = node;
+            else {//链表已经有数据
+                tail.next = node;//插入到链表尾部
+                node.pre = tail;
+                node.next = null;
+            }
+            tail = node;//尾指针指向当前插入的结点
+            capacity--;//容量减1
+        }
     }
 }
