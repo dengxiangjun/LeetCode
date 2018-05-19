8b1d4e6b5b0d21ec3ffc0f119cab4ef647ea61c1
dengxiangjun <437833417@qq.com>
2018-05-19 23:33:16 +0800

    DeleteOperationForTwoStrings

--- src/DeleteOperationForTwoStrings.java
+++ src/DeleteOperationForTwoStrings.java
@@ -27,7 +27,7 @@ public class DeleteOperationForTwoStrings {
         for (int j = 0; j <= n; j++) dp[0][j] = j;
         for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];//相等，
                 else dp[i][j] = Math.min(dp[i - 1][j],dp[i][j - 1]) + 1;
             }
         return dp[m][n];
--- src/DistinctSubsequences.java
+++ src/DistinctSubsequences.java
@@ -26,6 +26,10 @@ public class DistinctSubsequences {
             for (int j = 1; j <= n; j++) {
                 if (s.charAt(i - 1) != t.charAt(j - 1)) dp[i][j] = dp[i - 1][j];
                 else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                //如果S[i]==T[j]，那么dp[i][j] = dp[i-1][j-1] + dp[i-1][j]。
+                // 意思是：如果当前S[i]==T[j]，那么当前这个字母即可以保留也可以抛弃，
+                // 所以变换方法等于保留这个字母的变换方法加上不用这个字母的变换方法。
+                //如果S[i]!=T[i]，那么dp[i][j] = dp[i-1][j]，意思是如果当前字符不等，那么就只能抛弃当前这个字符。
             }
         return dp[m][n];
     }
